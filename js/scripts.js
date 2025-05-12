71818bae7239839e35ef3361e7846e4693a27d35
thaynaramabille <mabille.thata@gmail.com>
2025-05-12 00:20:47 -0300

    Versao intermediaria

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,16 @@ document.addEventListener("DOMContentLoaded", function () {
     multipleCardCarousel.classList.add("slide");
     }
 });
+
+
+<script>
+  function mostrarTexto() {
+    document.getElementById('texto-resumido').style.display = 'none';
+    document.getElementById('texto-completo-expandido').style.display = 'block';
+  }
+
+  function esconderTexto() {
+    document.getElementById('texto-resumido').style.display = 'block';
+    document.getElementById('texto-completo-expandido').style.display = 'none';
+  }
+</script>
