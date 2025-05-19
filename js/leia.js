20eda2ec731425d4fd10894a2776a6e950beb485
thaynaramabille <mabille.thata@gmail.com>
2025-05-19 00:14:31 -0300

    ajustes de margim

---
+++ js/leia.js
@@ -0,0 +1,11 @@
+<script>
+  function mostrarTexto() {
+    document.getElementById('texto-resumido').style.display = 'none';
+    document.getElementById('texto-completo-expandido').style.display = 'block';
+  }
+
+  function esconderTexto() {
+    document.getElementById('texto-resumido').style.display = 'block';
+    document.getElementById('texto-completo-expandido').style.display = 'none';
+  }
+</script>
